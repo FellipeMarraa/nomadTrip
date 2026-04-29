bc852c3902ffce8f4886471528ecde24c3efd20a
fellipe.marra <fellipe.marra@sankhya.com.br>
2026-04-29 10:51:05 -0300

    Ajustes label

--- src/pages/app/Dashboard.tsx
+++ src/pages/app/Dashboard.tsx
@@ -67,7 +67,7 @@ export function Dashboard() {
                             <span className="text-[10px] font-medium uppercase tracking-[0.15em]">
                                 {nextTripCountdown.days === 0
                                     ? "É hoje o seu embarque!"
-                                    : `Faltam ${nextTripCountdown.days} dias para ${nextTripCountdown.destination}`}
+                                    : `Falta pouco para sua viagem`}
                             </span>
                         </div>
                     )}
